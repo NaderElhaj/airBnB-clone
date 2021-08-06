58f3d9259a6e50ffa79c0f7d24af8d5246d80f06
Nader Elhaj <naderelhaj3@gmail.com>
2021-08-06 20:27:23 +0200

    Structured the Main Section And Pulled Data From the Server

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 
-export default function Home() {
+export default function Home({exploreData}) {
   return (
     <div className="">
       <Head>
@@ -11,6 +11,29 @@ export default function Home() {
       </Head>
       <Header/>
       <Banner />
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          
+          {/* Pull Data From Server */}
+          {exploreData?.map(item=>(
+            <h1>{item.location} </h1>
+          ))}
+
+        </section>
+      </main>
     </div>
   )
+}
+
+export async function getStaticProps(){
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+    res => res.json()
+    )
+
+    return{
+      props:{
+        exploreData
+      }
+    }
 }
